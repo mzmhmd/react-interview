0052cb01cf0679c5381e81d70523f5bd1159e705
mzmhmd <mzmuhammad106@gmail.com>
2025-06-18 00:40:29 +0530

    Image Slider - API Called

---
+++ image-slider/src/App.jsx
@@ -0,0 +1,15 @@
+import ImageSlider from '.';
+
+function App() {
+  return (
+    <>
+      <ImageSlider
+        url={'https://picsum.photos/v2/list'}
+        page={'1'}
+        limit={'10'}
+      />
+    </>
+  );
+}
+
+export default App;
